01ae6fd72cbcc693d9752faf1a0af5231495cab5
Oleh Sliusar <olehsliusar@gmail.com>
2017-03-29 18:38:22 +0300

    Change attributes of the Node class

--- node.rb
+++ node.rb
@@ -1,8 +1,9 @@
 class Node
-  attr_accessor :value, :parent, :child
-  def initialize(value, child = nil, parent = nil)
+  attr_accessor :value, :parent, :left, :right
+  def initialize(value, parent = nil, left = nil, right = nil)
     @value = value
     @parent = parent
-    @child = child
+    @left = left
+    @right = right
   end
 end
